3487bd6c5f0a6e2257566ec7771a83a9e38ba747
Amirul Ikmal <amirul.ikmal88@gmail.com>
2021-04-24 05:26:19 +0000

    lab2 commit

--- code2.2.c
+++ code2.2.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>	/* needed to define exit() */
 #include <unistd.h>	/* needed for fork() and getpid() */
 #include <stdio.h>	/* needed for printf() */
+#include <sys/wait.h>
 
 int main(int argc, char **argv) {
-
 	int pid;	/* process ID */
 
 	switch (pid = fork()) {
 	case 0:		/* a fork returns 0 to the child */
-		printf("I am the child process: pid=%d\n\n", getpid());
+		printf("I am the child process: pid=%d\n", getpid());
 		break;
 
 	default:	/* a fork returns a pid to the parent */
-		printf("\nI am the parent process: pid=%d, child pid=%d\n\n", getpid(), pid);
+		wait(NULL);
+		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
 		break;
 
 	case -1:	/* something went wrong */
@@ -21,3 +22,4 @@ int main(int argc, char **argv) {
 	}
 	exit(0);
 }
+
